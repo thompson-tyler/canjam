4ca6ea3b13ccad864d5ece27e7101a31d803800f
Roger Burtonpatel <roger.burtonpatel@tufts.edu>
2024-03-31 17:07:12 -0400

    Reasonable synth baby

--- melty-test.py
+++ melty-test.py
@@ -1,10 +1,21 @@
 import meltysynth as ms
+import pyaudio
+
+SAMPLE_RATE = 44100
+
+pa = pyaudio.PyAudio()
+strm = pa.open(
+    format = pyaudio.paInt16,
+    channels = 2,
+    rate = SAMPLE_RATE,
+    output = True)
+
 
 # Load the SoundFont.
-sound_font = ms.SoundFont.from_file("example.sf2")
+sound_font = ms.SoundFont.from_file("demo/example.sf2")
 
 # Create the synthesizer.
-settings = ms.SynthesizerSettings(44100)
+settings = ms.SynthesizerSettings(SAMPLE_RATE)
 synthesizer = ms.Synthesizer(sound_font, settings)
 
 # Play some notes (middle C, E, G).
